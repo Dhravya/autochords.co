0ec727c93daa23e59c3e75b8bfcde959cf29f0a7
Dhravya <dhravyashah@gmail.com>
2023-11-05 07:05:37 -0700

    temp. disable eslint rules

--- next.config.mjs
+++ next.config.mjs
@@ -5,6 +5,10 @@
 await import("./src/env.mjs");
 
 /** @type {import("next").NextConfig} */
-const config = {};
+const config = {
+    eslint: {
+        ignoreDuringBuilds: true,
+      },
+};
 
 export default config;
